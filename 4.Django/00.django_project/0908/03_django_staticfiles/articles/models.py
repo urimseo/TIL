25c7a27c061a988a8db2d59abdf258542219d3bd
urimseo <urim960417@gmail.com>
2021-09-08 23:48:41 +0900

    django

---
+++ 4.Django/00.django_project/0908/03_django_staticfiles/articles/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from imagekit.models import ImageSpecField, ProcessedImageFiled
+from imagekit.processors import ResizeToFill
+
+
+def articles_image_path(isntance, filename):
+    return f'user_{isntance.pk}/{filename}'
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    # image = models.ImageField(upload_to='images/', blank=True)
+    # image = models.ImageField(upload_to=articles_image_path, blank=True)
+    # image = ProcessedImageFiled(
+    #     upload_to='thumbnails/',
+    #     processors=[ResizeToFill(100, 50)],
+    #     format='JPEG',
+    #     options={'quality' : 60}
+    # )
+    
+    image = models.ImageField(upload_to='origins/', blank=True)
+    image_thumbnail = ImageSpecField(
+        source = 'image',
+        processors = [ResizeToFill(100, 50)],
+        format = 'JPEG',
+        options = { 'quality' : 90}
+    )
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
---
+++ 4.Django/00.django_project/0908/03_django_staticfiles/articles/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from .models import Article
+from .forms import ArticleForm
+
+# Create your views here.
+@require_safe
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES) # 이미지 파일과 같은파일 형식은 request.FILES에 들어있음 
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
+
+@require_safe
+def detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+@require_POST
+def delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES, instance=article) # 키워드 인자는 위치인자보다 앞에 올 수 없다. reqeust.FILES가 뒤에 오려면 files=request.FILES 로 작성해야한다 
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'article': article,
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
